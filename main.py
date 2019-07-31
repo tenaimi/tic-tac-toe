01aa09b6f38c2cb3200ff02f974dc8e83b07cba8
Tenaimi Okurude <tjokurude@gmail.com>
2019-07-31 18:48:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from src.game import Game
+#from src.game import Game
+import re
 
 def main():
-   game = Game()
-   game.start()
+    #game = Game()
+    #game.start()
+    reg = r'([0-2|\d][0-5?|\d][0-5?]\.?){4}'
+    ip1 = "192.168.0.255"
+    matched = re.match(reg, ip1)
+    if matched:
+        print(matched)
+    else:
+        print("no match")
+    
 main()
